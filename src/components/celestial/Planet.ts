7bfbd2594a5b77ff7dcad8b811fa721dd9e2616a
flmartineau <flavien.martineau@live.fr>
2023-04-01 17:12:50 +0200

    feat: add remaining planets

--- src/components/celestial/Planet.ts
+++ src/components/celestial/Planet.ts
@@ -1,4 +1,5 @@
 import { CelestialBody } from './CelestialBody';
+import * as THREE from 'three';
 
 
 
@@ -10,34 +11,55 @@ export class Planet extends CelestialBody {
 
 
     constructor(
-        name: string, 
-        radius: number, 
-        texturePath: string, 
-        rotationSpeed: number, 
-        mass: number, 
-        temperature: number, 
-        elapsedTime: number, 
+        name: string,
+        radius: number,
+        texturePath: string,
+        rotationSpeed: number,
+        mass: number,
+        temperature: number,
+        elapsedTime: number,
         distanceToSun: number,
         orbitSpeed: number,
         inclination: number) {
-            super(name, radius * 50, texturePath, rotationSpeed, mass, temperature, elapsedTime);
-            this.distanceToSun = distanceToSun;
-            this.orbitSpeed = orbitSpeed;
-            this.inclination = inclination;
+        super(name, radius * 50, texturePath, rotationSpeed, mass, temperature, elapsedTime);
+        this.distanceToSun = distanceToSun;
+        this.orbitSpeed = orbitSpeed;
+        this.inclination = inclination;
+    }
+
+    updateOrbit(deltaTime: number): void {
+        this.elapsedTime += deltaTime;
+        const angle = this.elapsedTime * this.orbitSpeed;
+
+        // Calculate the elliptical orbit
+        const a = this.distanceToSun;
+        const b = this.distanceToSun * 0.9; // Example eccentricity factor
+        const x = a * Math.cos(angle);
+        const z = b * Math.sin(angle);
+        const y = z * Math.tan(this.inclination);
+
+        this.position.set(x, y, z);
+    }
+
+    private createOrbitGeometry(segments: number = 100): THREE.BufferGeometry {
+        const points: THREE.Vector3[] = [];
+        for (let i = 0; i <= segments; i++) {
+            const theta = (i / segments) * Math.PI * 2;
+            const x = this.distanceToSun * Math.cos(theta);
+            const y = this.distanceToSun * Math.sin(theta) * Math.sin(this.inclination);
+            const z = this.distanceToSun * Math.sin(theta) * Math.cos(this.inclination);
+            points.push(new THREE.Vector3(x, y, z));
         }
 
-        updateOrbit(deltaTime: number): void {
-            this.elapsedTime += deltaTime;
-            const angle = this.elapsedTime * this.orbitSpeed;
-        
-            // Calculate the elliptical orbit
-            const a = this.distanceToSun;
-            const b = this.distanceToSun * 0.9; // Example eccentricity factor
-            const x = a * Math.cos(angle);
-            const z = b * Math.sin(angle);
-            const y = z * Math.tan(this.inclination);
-        
-            this.position.set(x, y, z);
-          }
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        return geometry;
+    }
+
+    createOrbitLine(): THREE.Line {
+        const orbitGeometry = this.createOrbitGeometry();
+        const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+        const orbitLine = new THREE.Line(orbitGeometry, orbitMaterial);
+        return orbitLine;
+    }
 
 }
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -37,24 +37,18 @@ export class MainScene {
 
 
   private sun: Sun;
+
+  //Planets
   private mercury: Mercury;
   private venus: Venus;
   private earth: Earth;
   private mars: Mars;
   private jupiter: Jupiter;
-  private saturn : Saturn;
-  private uranus : Uranus;
-  private neptune : Neptune;
+  private saturn: Saturn;
+  private uranus: Uranus;
+  private neptune: Neptune;
 
-
-  private mercuryOrbitLine: THREE.Line;
-  private venusOrbitLine: THREE.Line;
-  private jupiterOrbitLine: THREE.Line;
-  private earthOrbitLine: THREE.Line;
-  private marsOrbitLine: THREE.Line;
-  private saturnOrbitLine: THREE.Line;
-  private uranusOrbitLine: THREE.Line;
-  private neptuneOrbitLine: THREE.Line;
+  private planets: Array<Planet>;
 
   private controls: OrbitControls;
   private raycaster: Raycaster;
@@ -63,35 +57,37 @@ export class MainScene {
 
   private selectedObject: Mesh | null;
   private focusedObject: Mesh | null = null;
-private cameraOffset: Vector3 = new Vector3();
+  private cameraOffset: Vector3 = new Vector3();
 
   constructor(container: HTMLElement) {
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.selectedObject = null;
+
+
+
+    this.selectedObject = null;
     container.appendChild(this.renderer.domElement);
 
 
     // Add the click event listener
-   window.addEventListener('click', (event) => this.onClick(event), false);
+    window.addEventListener('click', (event) => this.onClick(event), false);
 
     // Load the skybox textures
-        const loader = new CubeTextureLoader();
-        this.skybox = loader.load([
-          '../../assets/textures/stars_2.jpg',
-          '../../assets/textures/stars_2.jpg',
-          '../../assets/textures/stars_2.jpg',
-          '../../assets/textures/stars_2.jpg',
-          '../../assets/textures/stars_2.jpg',
-          '../../assets/textures/stars_2.jpg'
-        ]);
-        this.skybox.encoding = THREE.sRGBEncoding;
+    const loader = new CubeTextureLoader();
+    this.skybox = loader.load([
+      '../../assets/textures/stars_2.jpg',
+      '../../assets/textures/stars_2.jpg',
+      '../../assets/textures/stars_2.jpg',
+      '../../assets/textures/stars_2.jpg',
+      '../../assets/textures/stars_2.jpg',
+      '../../assets/textures/stars_2.jpg'
+    ]);
+    this.skybox.encoding = THREE.sRGBEncoding;
 
-        // Set the scene background to the skybox
-      this.scene.background = this.skybox;
-
+    // Set the scene background to the skybox
+    this.scene.background = this.skybox;
 
     this.camera.position.z = 5;
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -109,43 +105,26 @@ private cameraOffset: Vector3 = new Vector3();
     this.scene.add(ambientLight);
 
     this.sun = new Sun();
-    this.scene.add(this.sun);
-
-   
-   this.mercury = new Mercury();
-   this.scene.add(this.mercury);
-   this.mercuryOrbitLine = this.createOrbitLine(this.mercury);
-
-   this.venus = new Venus();
-   this.scene.add(this.venus);
-   this.venusOrbitLine = this.createOrbitLine(this.venus);
-
-   this.earth = new Earth();
-   this.scene.add(this.earth);
-   this.earthOrbitLine = this.createOrbitLine(this.earth);
-
-   this.mars = new Mars();
-    this.scene.add(this.mars);
-  this.marsOrbitLine = this.createOrbitLine(this.mars);
-
-   this.jupiter = new Jupiter()
-   this.scene.add(this.jupiter);
-   this.jupiterOrbitLine = this.createOrbitLine(this.jupiter);
-
+    this.mercury = new Mercury();
+    this.venus = new Venus();
+    this.earth = new Earth();
+    this.mars = new Mars();
+    this.jupiter = new Jupiter()
     this.saturn = new Saturn()
-    this.scene.add(this.saturn);
-    this.saturnOrbitLine = this.createOrbitLine(this.saturn);
-
     this.uranus = new Uranus()
-    this.scene.add(this.uranus);
-    this.uranusOrbitLine = this.createOrbitLine(this.uranus);
-
     this.neptune = new Neptune()
-    this.scene.add(this.neptune);
-    this.neptuneOrbitLine = this.createOrbitLine(this.neptune);
 
 
+    this.scene.add(this.sun);
+    
+    this.planets = [this.mercury, this.venus, this.earth, this.mars, this.jupiter, this.saturn, this.uranus, this.neptune];
 
+    this.planets.forEach((planet: Planet) => {
+      this.scene.add(planet);
+      this.scene.add(planet.createOrbitLine());
+    });
+
+  
     this.raycaster = new Raycaster();
     this.mouse = new Vector2();
 
@@ -156,106 +135,69 @@ private cameraOffset: Vector3 = new Vector3();
   }
 
   private onClick(event: MouseEvent): void {
-  this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  this.raycaster.setFromCamera(this.mouse, this.camera);
+    this.raycaster.setFromCamera(this.mouse, this.camera);
 
-  const celestialObjects = [this.sun, this.mercury, this.venus, this.earth, this.mars, this.jupiter, this.saturn, this.uranus, this.neptune]; // Add more planets to this array as needed
-  const intersects = this.raycaster.intersectObjects(celestialObjects);
+    const celestialObjects = [this.sun, this.mercury, this.venus, this.earth, this.mars, this.jupiter, this.saturn, this.uranus, this.neptune]; // Add more planets to this array as needed
+    const intersects = this.raycaster.intersectObjects(celestialObjects);
 
-  if (intersects.length > 0) {
-    const object = intersects[0].object;
-    this.selectedObject = object as Mesh;
-    this.centerCameraOnObject(this.selectedObject);
-  }
-}
-
-private onControlsChange(): void {
-  if (this.focusedObject) {
-    this.cameraOffset.copy(this.camera.position).sub(this.focusedObject.position);
-  }
-}
-
-
-private centerCameraOnObject(object: Mesh): void {
-  const initialDistance = this.camera.position.distanceTo(object.position);
-
-  this.controls.target.copy(object.position);
-  this.controls.update();
-
-  const newDistance = this.camera.position.distanceTo(object.position);
-  const scalingFactor = initialDistance / newDistance;
-  const newPosition = this.camera.position.clone().sub(object.position).multiplyScalar(scalingFactor).add(object.position);
-  this.camera.position.copy(newPosition);
-  this.controls.update();
-
-  this.focusedObject = object;
-  this.cameraOffset.copy(this.camera.position).sub(object.position);
-}
-
-
-
-private createOrbitGeometry(distanceToSun: number, inclination: number, segments: number = 100): THREE.BufferGeometry {
-  const points: THREE.Vector3[] = [];
-  for (let i = 0; i <= segments; i++) {
-    const theta = (i / segments) * Math.PI * 2;
-    const x = distanceToSun * Math.cos(theta);
-    const y = distanceToSun * Math.sin(theta) * Math.sin(inclination);
-    const z = distanceToSun * Math.sin(theta) * Math.cos(inclination);
-    points.push(new THREE.Vector3(x, y, z));
+    if (intersects.length > 0) {
+      const object = intersects[0].object;
+      this.selectedObject = object as Mesh;
+      this.centerCameraOnObject(this.selectedObject);
+    }
   }
 
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  return geometry;
-}
+  private onControlsChange(): void {
+    if (this.focusedObject) {
+      this.cameraOffset.copy(this.camera.position).sub(this.focusedObject.position);
+    }
+  }
 
 
-private createOrbitLine(planet: Planet): THREE.Line {
-  const orbitGeometry = this.createOrbitGeometry(planet.distanceToSun, planet.inclination);
-  const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
-  const orbitLine = new THREE.Line(orbitGeometry, orbitMaterial);
-  this.scene.add(orbitLine);
-  return orbitLine;
-}
+  private centerCameraOnObject(object: Mesh): void {
+    const initialDistance = this.camera.position.distanceTo(object.position);
 
+    this.controls.target.copy(object.position);
+    this.controls.update();
 
+    const newDistance = this.camera.position.distanceTo(object.position);
+    const scalingFactor = initialDistance / newDistance;
+    const newPosition = this.camera.position.clone().sub(object.position).multiplyScalar(scalingFactor).add(object.position);
+    this.camera.position.copy(newPosition);
+    this.controls.update();
 
+    this.focusedObject = object;
+    this.cameraOffset.copy(this.camera.position).sub(object.position);
+  }
 
   private animate(): void {
     requestAnimationFrame(() => this.animate());
-
+    const deltaTime = 0.016; // Use a fixed time step or calculate the elapsed time since the last frame
 
 
     this.sun.rotation.y += this.sun.rotationSpeed;
-    this.mercury.rotation.y += this.mercury.rotationSpeed;
-    this.jupiter.rotation.y += this.jupiter.rotationSpeed;
-    this.venus.rotation.y += this.venus.rotationSpeed;
-    this.earth.rotation.y += this.earth.rotationSpeed;
-    this.mars.rotation.y += this.mars.rotationSpeed;
-    this.saturn.rotation.y += this.saturn.rotationSpeed;
-    this.uranus.rotation.y += this.uranus.rotationSpeed;
-    this.neptune.rotation.y += this.neptune.rotationSpeed;
+
+
+    this.planets.forEach((planet: Planet) => {
+      planet.rotation.y += planet.rotationSpeed;
+      planet.updateOrbit(deltaTime);
+    });
+
+    
     if (this.focusedObject) {
-    this.camera.position.copy(this.focusedObject.position).add(this.cameraOffset);
-    this.controls.target.copy(this.focusedObject.position);
-    this.controls.update();
-  }
+      this.camera.position.copy(this.focusedObject.position).add(this.cameraOffset);
+      this.controls.target.copy(this.focusedObject.position);
+      this.controls.update();
+    }
 
 
     this.renderer.render(this.scene, this.camera);
     this.controls.update();
 
-    // Update Mercury's orbit
-   const deltaTime = 0.016; // Use a fixed time step or calculate the elapsed time since the last frame
-   this.mercury.updateOrbit(deltaTime);
-   this.venus.updateOrbit(deltaTime);
-   this.earth.updateOrbit(deltaTime);
-   this.mars.updateOrbit(deltaTime);
-   this.jupiter.updateOrbit(deltaTime);
-    this.saturn.updateOrbit(deltaTime);
-    this.uranus.updateOrbit(deltaTime);
-    this.neptune.updateOrbit(deltaTime);
+
   }
 
   private onMouseMove(event: MouseEvent): void {
@@ -264,8 +206,8 @@ private createOrbitLine(planet: Planet): THREE.Line {
 
     this.raycaster.setFromCamera(this.mouse, this.camera);
 
-    const intersects = this.raycaster.intersectObjects([this.sun, this.mercury, this.venus, this.earth, 
-      this.mars, this.jupiter, this.saturn, this.uranus, this.neptune]);
+    const intersects = this.raycaster.intersectObjects([this.sun, this.mercury, this.venus, this.earth,
+    this.mars, this.jupiter, this.saturn, this.uranus, this.neptune]);
 
 
     if (intersects.length > 0) {
